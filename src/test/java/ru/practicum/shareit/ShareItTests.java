074858a03c9f723b66d5d6426bc9785ba7794615
vassiliy <egorovwa2@gmail.com>
2022-07-20 14:06:33 +0700

    codeStyle

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.practicum.shareit.exceptions.IncorrectUserIdException;
 import ru.practicum.shareit.exceptions.ModelAlreadyExistsException;
-import ru.practicum.shareit.exceptions.ModelNotExitsException;
 import ru.practicum.shareit.item.ItemServise;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
@@ -14,31 +13,31 @@ import ru.practicum.shareit.user.UserServise;
 
 import java.util.Collection;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class ShareItTests {
-	private final UserServise userServise;
-	private final ItemServise itemServise;
-	private User user1 = new User(null,"user1@nfff.com","user1");
-	private User user2 = new User(null,"user2@nfff.com","user2");
-	Item item = new Item(null,"name rrrr", "word finded word",true, user1);
-	@Test
-	void contextLoads() {
-	}
-	@Test
-	void test1_findItemByText() throws ModelAlreadyExistsException, IncorrectUserIdException {
-		user1 = userServise.addUser(user1);
-		itemServise.createItem(user1.getId(),item);
+    private final UserServise userServise;
+    private final ItemServise itemServise;
+    private User user1 = new User(null, "user1@nfff.com", "user1");
+    private User user2 = new User(null, "user2@nfff.com", "user2");
+    Item item = new Item(null, "name rrrr", "word finded word", true, user1);
 
-		Collection<Item> itemList = itemServise.findByText("finded");
-		assertEquals(1, itemList.size());
-		assertEquals(itemList.stream().findFirst().get().getName(), item.getName());
-	}
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void test1_findItemByText() throws ModelAlreadyExistsException, IncorrectUserIdException {
+        user1 = userServise.addUser(user1);
+        itemServise.createItem(user1.getId(), item);
+
+        Collection<Item> itemList = itemServise.findByText("finded");
+        assertEquals(1, itemList.size());
+        assertEquals(itemList.stream().findFirst().get().getName(), item.getName());
+    }
+}
 
 
