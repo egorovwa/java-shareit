0bee51b019d089c63d3cb7d3f4e38ce3ae09a00c
vasiliy <egorovwa2@gmail.com>
2022-08-26 14:05:52 +0700

    Repository tests

--- src/main/java/ru/practicum/shareit/booking/exceptions/WrongUserIdException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.booking.exceptions;
-
-import ru.practicum.shareit.exceptions.IncorrectUserIdException;
-
-public class WrongUserIdException extends IncorrectUserIdException {
-    public WrongUserIdException(String message, String userId) {
-        super(message, userId);
-    }
-}
