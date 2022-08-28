0157ff9add0611dd045a6d4dd6ffead58d02a4fe
vasiliy <egorovwa2@gmail.com>
2022-08-28 19:17:44 +0700

    TestConstants rename

--- src/test/java/ru/practicum/shareit/booking/dto/BookingDtoMaperTest.java
+++ src/test/java/ru/practicum/shareit/booking/dto/BookingDtoMaperTest.java
@@ -41,7 +41,9 @@ class BookingDtoMaperTest {
 
     @Test
     void toDto() {
-        Booking booking = BOOKING1_USER2_ITEM1_WAITING;
+        Booking booking = new Booking(1L, LocalDateTime.now().toEpochSecond(ZoneOffset.UTC),
+                LocalDateTime.now().plus(Duration.ofHours(1)).toEpochSecond(ZoneOffset.UTC), ITEM_ID1_OWNER1_AVALIBLE_TRUE,
+                USER_ID2, BookingStatus.WAITING);
         BookingDto bookingDto =
                 new BookingDto(1L, LocalDateTime.ofEpochSecond(booking.getStart(), 0, ZoneOffset.UTC),
                         LocalDateTime.ofEpochSecond(booking.getEnd(), 0, ZoneOffset.UTC), booking.getItem(),
